d626eb1c1afc8c79ac8f943caa2ad5dd54efee99
Vincent Perrin <vincent.perrin@gmail.com>
2018-06-28 13:16:27 +0200

    first commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	public bool IsGrounded = false;
+	public GameObject RotorMain;
+	public GameObject RotorRear;
+
+	int RotorSpeed = 800;
+
+	// Use this for initialization
+	void Start () {
+		RotorMain = GameObject.Find("Rotor");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RotorMain.transform.Rotate(new Vector3(0,0,1) * RotorSpeed * Time.deltaTime);
+
+		/*if(Input.GetKey (KeyCode.LeftShift))
+			{
+				RotorSpeed += 10;
+				RotorSpeed = Mathf.Clamp (RotorSpeed, 800, 1200);
+			transform.Translate(new Vector3(0,1,0) * 2 * Time.deltaTime);
+
+			}
+		if(Input.GetKey (KeyCode.LeftControl))
+		{
+			RotorSpeed -= 10;
+			RotorSpeed = Mathf.Clamp (RotorSpeed, 800, 1200);
+			transform.Translate(-new Vector3(0,1,0) * 2 * Time.deltaTime);
+
+		}
+
+		if (!IsGrounded) 
+		{
+			transform.Rotate (Vector3.up * Input.GetAxis ("Horizontal") * Time.deltaTime * RotorSpeed / 10);
+			transform.Translate (-Vector3.forward * Input.GetAxis ("Vertical") * Time.deltaTime * RotorSpeed / 100);
+		}*/
+	}
+}
